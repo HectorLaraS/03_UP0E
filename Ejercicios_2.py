bb505d34f2e861d75b457a1cef36196d87a4cd93
hectorlaras <hector.laras18@outlook.com>
2025-09-20 16:50:34 -0600

    more infor

--- Ejercicios_2.py
+++ Ejercicios_2.py
@@ -2,3 +2,13 @@
 #Valores cadenas de texto str
 #Boolean valore logico True or False
 #None es una ausencia de valor
+edad = 30
+altura = 1.75
+cadena_texto = "Hecto Humberto Lara Salas"
+es_casado = True
+permisos_root = True
+es_ingeniero_principal = True
+es_vip = False
+
+#None ausencia de valor
+costo_final = None

a50093a31c30ea8f1f3026a7bafc6e45374a40ae
hectorlaras <hector.laras18@outlook.com>
2025-09-20 16:30:13 -0600

    json reader added

--- Ejercicios_1.py
+++ Ejercicios_1.py
@@ -1,5 +1,15 @@
+import json
 name = "Hector Lara"
 age = 32
 country = "Mexico"
 full_info = f"{name}, {age}, {country}"
 print(full_info)
+is_married = True
+
+file_path = "data.json"
+try:
+    with open(file_path, "r") as file:
+        data = json.load(file)
+        print(data["Person Information"]["Name"])
+except FileNotFoundError:
+    print("File not found")

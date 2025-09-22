af144040e59bbac10edb0a991961991d8d5f6b4c
hectorlaras <hector.laras18@outlook.com>
2025-09-22 17:32:20 -0600

    Clase constantes

---
+++ contantes.py
@@ -0,0 +1,9 @@
+"""
+en Python siempre es posible moficar los valores de la variable y no hay una manera nativa de crear constantes
+por buena practica lo que se hace es poner la variable en MAYUSCULAS
+"""
+import math
+PI = 3.14159
+MENSAJE_ERROR_CE_001 = "ERROR MECANICO EN LA MAQUINA DE ROTACION. LLAMAR A MTTO"
+print(MENSAJE_ERROR_CE_001)
+print(math.pi)
